b55e4db3472c9a802c1c156c68b9bd64f434dce6
faul <faullik@gmail.com>
2015-09-27 18:43:26 +0300

    Updated datamapper adapter. might work.

--- lib/seo_app/adapters/datamapper.rb
+++ lib/seo_app/adapters/datamapper.rb
@@ -20,37 +20,36 @@ module SeoApp
       end
 
       def report(_key)
-        # _report = @reports[_key]
-        # _result = { report: _report, links: _report.links_dataset.all,
-        #             headers: _report.headers_dataset.all }
-        # SeoApp::Generator.to_html(_result)
+        _report = @reports.get(_key)
+        _result = { report: _report, links: _report.links.all,
+                    headers: _report.headers.all }
+        SeoApp::Generator.to_html(_result)
       end
 
       def save_report(_opts)
-        # @conn.transaction do
-        #   _report = @reports.create(url: _opts[:url].to_s, created_at: _opts[:date],
-        #                             links_count: _opts[:links].count,
-        #                             remote_ip: _opts[:geo]['ip'],
-        #                             country: _opts[:geo]['country_name'])
+        _report = @reports.create(url: _opts[:url].to_s, created_at: _opts[:date],
+                                  links_count: _opts[:links].count,
+                                  remote_ip: _opts[:geo]['ip'],
+                                  country: _opts[:geo]['country_name'])
 
-        #   save_links(_report, _opts[:links])
-        #   save_headers(_report, _opts[:headers])
-        # end
+        save_links(_report, _opts[:links])
+        save_headers(_report, _opts[:headers])
+        _report.save
       end
 
       def save_links(_report, _links)
-        # _links.each do |link|
-        #   _link = @links.create(name: link[:name], href: link[:url],
-        #                         rel: link[:rel], target: link[:target])
-        #   _report.add_link(_link)
-        # end
+        _links.each do |link|
+          _link = @links.create(name: link[:name], href: link[:url],
+                                rel: link[:rel], target: link[:target])
+          _report << _link
+        end
       end
 
       def save_headers(_report, _headers)
-        # _headers.each do |k, v|
-        #   _header = @headers.create(name: k, value: v)
-        #   _report.add_header(_header)
-        # end
+        _headers.each do |k, v|
+          _header = @headers.create(name: k, value: v)
+          _report << _header
+        end
       end
 
       def connect_with_creds
